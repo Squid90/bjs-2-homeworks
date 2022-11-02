bfbd09f9076dbf3be58bee4a342c1a5666d4a5db
Ivan Romaniuk <squid90@rambler.ru>
2022-11-02 22:54:27 +0300

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,14 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  "use strict";
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
   return arr; // array
 }
 
