54a57d5c5c936d6cfeee0cc0f470c391cb26fc6b
Ivan Romaniuk <squid90@rambler.ru>
2022-11-13 17:24:20 +0300

    Task #3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,26 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  
+    if (arr1.length === arr2.length) {
+      for(let i = 0; i < arr1.length; i += 1) {
+        if (arr1[i] === arr2[i]) {
+          result = true;
+         } else {
+          result = false;
+         }
+       }
+    } else {
+      result = false;
+    }
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = [];
 
-  // Ваш код
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item * 10);
 
   return resultArr; // array
 }
